b008bb4c8be4e99699b637b0c4e079c2700a64ef
Yo-An Lin (林佑安) <cornelius@cpan.org>
2010-08-09 17:05:38 +0000

    Import of CORNELIUS/App-gh-0.111 from CPAN.
    gitpan-cpan-distribution: App-gh
    gitpan-cpan-version:      0.111
    gitpan-cpan-path:         CORNELIUS/App-gh-0.111.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

---
+++ lib/App/gh/Command/Pull.pm
@@ -0,0 +1,38 @@
+package App::gh::Command::Pull;
+use warnings;
+use strict;
+use base qw(App::gh::Command);
+use App::gh::Utils;
+
+
+sub parse_remote_param {
+    my $uri = shift;
+    if ( $uri =~ m{(?:git|https?)://github.com/(.*?)/(.*?).git} 
+        || $uri =~ m{git\@github.com:(.*?)/(.*?).git} ) 
+    {
+        return ( $1 , $2 )
+            if( $1 && $2 );
+    }
+    return undef;
+}
+
+sub run {
+    my $self = shift;
+    my $acc  = shift;
+    my $branch = shift || 'master';
+
+    die "git config not found." if  ! -e ".git/config" ;
+    my $config = parse_config( ".git/config" );
+
+    # git://github.com/miyagawa/Tatsumaki.git
+    for my $remote ( values %{ $config->{remote} } ) {
+        if( my ($my, $repo) = parse_remote_param( $remote->{url} ) )
+        {
+            my $uri = sprintf( qq(git://github.com/%s/%s.git) , $acc , $repo );
+            qx(git pull $uri $branch);
+            last;
+        }
+    }
+}
+
+1;
